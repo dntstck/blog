f585e7ce0d0d8cd309cd37f531f56e965a41bcb1
dntstck <drudelarosa@icloud.com>
2025-01-08 02:53:47 +0000

    revert

--- deploy/schedulePosts.js
+++ deploy/schedulePosts.js
@@ -15,22 +15,6 @@ console.log(`Publish Directory: ${publishDir}`);
 console.log(`Scheduled Directory: ${scheduledDir}`);
 console.log(`Index File Path: ${indexFilePath}`);
 
-function getDirectoryByTag(tag) {
-  const tagDirectoryMap = {
-    'cm5': 'cm5', 
-    'devserver': 'devserver',
-    'embedded': 'embeddedc',
-    'microcontrollers': 'microcontrollers',
-    'misc': 'misc',
-    'osnetworking': 'osnetworking', 
-    'picosystem': 'picosystem',
-    'raspberrypi': 'raspberrypi',
-    'thoughts': 'thoughts',
-    'webdevelopment': 'webdev'
-  };
-  return tagDirectoryMap[tag] || 'misc';
-}
-
 try {
   if (!fs.existsSync(scheduledDir)) {
     console.error(`Scheduled directory does not exist: ${scheduledDir}`);
@@ -76,7 +60,7 @@ try {
             console.log(`Tags: ${tags}`);
 
             tags.forEach(tag => {
-              const tagDir = path.join(publishDir, getDirectoryByTag(tag.toLowerCase()));
+              const tagDir = path.join(publishDir, tag.toLowerCase());
               if (!fs.existsSync(tagDir)) {
                 console.log(`Creating directory: ${tagDir}`);
                 fs.mkdirSync(tagDir, { recursive: true });
@@ -123,12 +107,9 @@ try {
     }
   });
 
+  // update index.md
   let indexContent = fs.readFileSync(indexFilePath, 'utf8');
-  const latestPostsSection = latestPosts.map(post => {
-    const directory = getDirectoryByTag(post.tag);
-    const badgeUrl = `https://img.shields.io/badge/${encodeURIComponent(post.title)}-151515?style=flat-square`;
-    return `<a href="./${directory}/${post.file.replace('.md', '.html')}"><img src="${badgeUrl}" alt="${post.title}"></a><br>`;
-  }).join('\n');
+  const latestPostsSection = latestPosts.map(post => `- [${post.title}](./misc/${post.file})`).join('\n');
   console.log(`Latest Posts Section: \n${latestPostsSection}`);
 
   indexContent = indexContent.replace(
@@ -139,6 +120,7 @@ try {
   fs.writeFileSync(indexFilePath, indexContent);
   console.log('Updated index.md with latest posts.');
 
+  // ensure the scheduled dir remains
   if (fs.readdirSync(scheduledDir).length === 0) {
     console.log(`Scheduled directory is empty. Adding a placeholder file to keep the directory.`);
     fs.writeFileSync(path.join(scheduledDir, '.gitkeep'), '');
--- misc/test.md
+++ misc/test.md
@@ -1,8 +1,6 @@
 ---
 title: "Test Post2"
 date: 2025-01-07T02:05:00Z
-publishDate: 2025-01-07T20:35:00Z
+publishDate: 2025-01-07T02:25:00Z  
 tags: [Misc]
 ---
-
-This is a sample post content.
