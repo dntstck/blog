fe97f8b3249949dba3cb5833bf3b2f1c19dad368
dntstck <drudelarosa@icloud.com>
2025-01-27 07:44:25 +0000

    bash script updated

--- new.sh
+++ new.sh
@@ -34,30 +34,8 @@ publishDate: $PUBLISH_DATE
 tags: [$TAGS]
 ---
 
-<style> 
-body, .content { 
-  word-wrap: break-word;
-  overflow-wrap: break-word; 
-  box-sizing: border-box;
-  } /* fix blog text overflowing */ 
-#blog-post { 
-  max-width: 80%;
-  margin: 0 auto; 
-  padding: 0 1rem; 
-  word-wrap: break-word;
-  overflow-wrap: break-word;
-  box-sizing: border-box;
-  } 
-hr {
-  max-width: 100%
-}
-</style>
-<div id="blog-post">
-
 Blog Content here. 
 
-</div>
-<br>
 EOF
 
 echo "$TITLE.md generated at $DIRECTORY/$FILENAME"
