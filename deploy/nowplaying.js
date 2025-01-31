acaf62ca8661ddce94db9ce43742407c5f770221
dntstck <dntstck@icloud.com>
2025-01-31 00:00:27 +0000

    nowplaying.js added

---
+++ deploy/nowplaying.js
@@ -0,0 +1,12 @@
+async function fetchNowPlaying() {
+  const username = "dntstck";
+  const apiKey = "17d7b04ecdaee026c8eadf2e1e02364c";
+  const response = await fetch(
+    `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json`
+  );
+  const data = await response.json();
+  const nowPlaying = data.recenttracks.track[0];
+  const trackInfo = `${nowPlaying.artist["#text"]} - ${nowPlaying.name}`;
+  document.getElementById("now-playing").innerText = trackInfo;
+}
+fetchNowPlaying();
