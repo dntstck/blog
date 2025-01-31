b0f77ec5b7137ec41cac619577bb2c4238494517
dntstck <dntstck@icloud.com>
2025-01-31 01:01:50 +0000

    nowplaying.js updated

--- deploy/nowplaying.js
+++ deploy/nowplaying.js
@@ -1,4 +1,4 @@
-async function fetchNowPlaying() {
+/* async function fetchNowPlaying() {
   const username = "dntstck";
   const apiKey = "17d7b04ecdaee026c8eadf2e1e02364c";
   const response = await fetch(
@@ -6,8 +6,32 @@ async function fetchNowPlaying() {
   );
   const data = await response.json();
   const nowPlaying = data.recenttracks.track[0];
+  const trackInfo = `${nowPlaying.artist["#text"]}%20-%20${nowPlaying.name}`;
+  document.getElementById("now-playing").innerHTML = `<img alt=\"Now Playing\" src=\"https://img.shields.io/badge/-${trackInfo}-151515?&logo=vlcmediaplayer&logoColor=black\">`;
+}
+fetchNowPlaying();
+
+*/
+
+async function fetchNowPlaying() {
+  const username = "dntstck";
+  const apiKey = "17d7b04ecdaee026c8eadf2e1e02364c";
+  const response = await fetch(
+    `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json`
+  );
+  const data = await response.json();
+  
+  console.log(data); // log response
+  
+  const nowPlaying = data.recenttracks.track[0];
+  console.log(nowPlaying); // Log the now playing track
+  
   const trackInfo = `${nowPlaying.artist["#text"]} - ${nowPlaying.name}`;
   const encodedTrackInfo = encodeURIComponent(trackInfo);
+  
+  console.log(encodedTrackInfo); // log track info
+  
   document.getElementById("now-playing").innerHTML = `<img alt="Now Playing" src="https://img.shields.io/badge/-${encodedTrackInfo}-151515?&logo=vlcmediaplayer&logoColor=black">`;
 }
+
 fetchNowPlaying();
