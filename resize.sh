3d053432648fd56fc32d78ee37e2579de5ef3426
dntstck <dntstck@icloud.com>
2026-07-27 10:41:09 +0100

    refined css rules for mobile devices - includes img

--- resize.sh
+++ resize.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-# get dir
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-# Output directory for resized images
-OUTPUT_DIR="$SCRIPT_DIR/resized"
 
-# create output dir if it doesn't exist
-mkdir -p "$OUTPUT_DIR"
+INPUT_DIR="$SCRIPT_DIR/raw_img"
+OUTPUT_DIR="$SCRIPT_DIR/assets/img"
 
-# loop through each img
-for IMAGE in "$SCRIPT_DIR"/*.{jpg,jpeg,png}; do
-  # Check if the file exists
+# loop thru
+for IMAGE in $INPUT_DIR/*.{jpg,jpeg,png}; do
   if [[ -f "$IMAGE" ]]; then
-    # Get the base name of the image file
     BASENAME=$(basename "$IMAGE")
-    # Resize the image to 480x320 pixels
     convert "$IMAGE" -resize 480x320\! "$OUTPUT_DIR/$BASENAME"
-    
-    echo "Resized $IMAGE and saved to $OUTPUT_DIR/$BASENAME"
+    echo "Resized $IMAGE → $OUTPUT_DIR/$BASENAME"
   fi
 done
 
 echo "Resize successful."
-
